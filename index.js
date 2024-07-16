b8d83af98f3421d9f3aeed7d39d9d5e9493ae3c5
Yash Mahindrakar <yash.mahindrakar@einfochips.com>
2024-07-16 11:10:57 +0530

    Update main route message

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Kubernetes!');
-});
-
-app.listen(port, () => {
-    console.log(`App running at http://localhost:${port}`);
+    res.send('Hello, Kubernetes! Updated version.');
 });
 
 app.get('/newroute', (req, res) => {
     res.send('This is a new route!');
+});
+
+app.listen(port, () => {
+    console.log(`App running at http://localhost:${port}`);
 });
